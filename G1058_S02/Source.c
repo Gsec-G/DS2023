e0c8ceacd18cb0f107282b73ac6093c151d2825b
Mihai Doinea <mihai.doinea@oracle.com>
2023-03-07 08:28:22 +0200

    G1058_S03

--- G1058_S02/Source.c
+++ G1058_S02/Source.c
@@ -2,28 +2,52 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
+#include <stdlib.h>
+
+union Referinta
+{
+	char refIntern;
+	short refExtern;
+};
+typedef union Referinta RefInfo;
 struct Angajat
 {
 	char* nume;
 	double salariu;
+	RefInfo referinta;
 };
-
 typedef struct Angajat AngajatInfo;
 //management info utila stocata in HEAP
-AngajatInfo* creareAngajat(char*, double);
+AngajatInfo* creareAngajat(char*, double, short);
 void dezalocareAngajat(AngajatInfo*);
-
+#define LINE_BUFFER 128
 void main()
 {
 	AngajatInfo angajat;
 	angajat.nume = NULL;
 	angajat.salariu = 100.4;
 	AngajatInfo* agenda[10];
-
+	char fileLineBuffer[LINE_BUFFER];
 	FILE* pFile = fopen("Data.txt", "r");
+	char* token; short referinta; double salariu;
+	char nume[LINE_BUFFER];
+	char separator[] = "\n,";
 	if (pFile)
 	{
-		AngajatInfo* ang = creareAngajat("Popescu Maria", 1200.0);
+		while (fgets(fileLineBuffer, sizeof(fileLineBuffer), pFile))
+		{
+			//citire nume
+			token = strtok(fileLineBuffer, separator);
+			strcpy(nume, token);
+			//citire salariu
+			token = strtok(NULL, separator);
+			salariu = atof(token);
+			//citire referinta
+			token = strtok(NULL, separator);
+			referinta = atoi(token);
+			AngajatInfo* ang = creareAngajat(nume, salariu, referinta);
+		}
+		
 		fclose(pFile);
 	}
 }
@@ -37,7 +61,7 @@ void dezalocareAngajat(AngajatInfo* angajat)
 	}
 }
 
-AngajatInfo* creareAngajat(char* nume, double salariu)
+AngajatInfo* creareAngajat(char* nume, double salariu, short referinta)
 {
 	AngajatInfo* result = NULL;
 	
@@ -45,6 +69,6 @@ AngajatInfo* creareAngajat(char* nume, double salariu)
 	result->nume = (char*)malloc(strlen(nume) + 1);
 	strcpy(result->nume, nume);
 	result->salariu = salariu;
-
+	result->referinta.refExtern = referinta;
 	return result;
 }
